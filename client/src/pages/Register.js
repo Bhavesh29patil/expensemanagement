f670fc4b2c9fe3fd54d0c17124e43d36f54f2fd7
YashCyanide <103525833+YashCyanide@users.noreply.github.com>
2023-04-29 13:20:12 +0530

    second version

---
+++ client/src/pages/Register.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <>
+     <div className='register-page'>
+        <h1>login page</h1>
+     </div>
+    </>
+  )
+}
+
+export default Register
